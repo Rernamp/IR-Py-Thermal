eedbf14f7be1d76b614b7d981465d75bdf2e0cf6
Rernamp <renamp100@gmail.com>
2026-02-19 12:42:42 +0300

    add ps1 script for take periodical photo

---
+++ periodicalTakePhoto.ps1
@@ -0,0 +1,18 @@
+param(
+    [Parameter(Mandatory=$false, HelpMessage="Samples count")]
+    [Alias("c")]
+    [int]$SamplesCount = 10,
+    [Parameter(Mandatory=$false, HelpMessage="Resulted folder")]
+    [Alias("f")]
+    [string]$folder,
+    [Parameter(Mandatory=$false, HelpMessage="Sleep between photo")]
+    [Alias("d")]
+    [int]$sleep_ms = 10
+)
+
+# $sleep_ms = 30 * 1000
+
+for ($i = 1; $i -le $SamplesCount; $i++) {
+    poetry run python ./takeSinglePhoto.py -f $folder
+    Start-Sleep -Milliseconds $sleep_ms
+}
